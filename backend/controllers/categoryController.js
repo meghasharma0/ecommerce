5bcc89f96ddd343f00ac49e2b378b7eff3ff29d0
Megha <meghaalg7@gmail.com>
2024-07-02 14:23:31 +0530

    update category

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("../middlewares/asyncHandler");
 const Category = require("../models/categoryModel");
 
+// CREATE CATEGORY
 const createCategory = asyncHandler(async (req, res) => {
   try {
     const { name } = req.body;
@@ -11,16 +12,32 @@ const createCategory = asyncHandler(async (req, res) => {
 
     const existingCategory = await Category.findOne({ name });
     if (existingCategory) {
-      return res.json({error: 'Already exists'});
+      return res.json({ error: "Already exists" });
     }
 
     const newCategory = await new Category({ name }).save();
     res.json(newCategory);
-
   } catch (error) {
     console.log(error);
     return res.status(400).json(error);
   }
 });
 
-module.exports = createCategory;
+// UPDATE CATEGORY
+const updateCategory = asyncHandler(async (req, res) => {
+    const {name} = req.body;
+    const {categoryId} = req.params;
+
+    const category = await Category.findOne({_id: categoryId});
+
+    if (!category) {
+        return res.status(404).json({error: 'Category not found!'});
+    }
+
+    category.name = name || category.name;
+
+    const updatedCategory = await category.save();
+    res.json(updatedCategory);
+});
+
+module.exports = { createCategory, updateCategory };
--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const { authenticate, authorizeAdmin } = require('../middlewares/authMiddleware');
-const createCategory = require('../controllers/categoryController');
+const express = require("express");
+const {
+  authenticate,
+  authorizeAdmin,
+} = require("../middlewares/authMiddleware");
+const {
+  createCategory,
+  updateCategory,
+} = require("../controllers/categoryController");
 
 const router = express.Router();
 
-router.route('/').post(authenticate, authorizeAdmin, createCategory);
+router.route("/").post(authenticate, authorizeAdmin, createCategory);
+router.route("/:categoryId").put(authenticate, authorizeAdmin, updateCategory);
 
 module.exports = router;
