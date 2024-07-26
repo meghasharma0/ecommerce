4d2ad7c29027d2f22515af9368504c86d25b4e49
Megha <meghaalg7@gmail.com>
2024-07-26 15:38:46 +0530

    remove, get all and single product functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -34,7 +34,6 @@ const addProduct = asyncHandler(async (req, res) => {
 // UPDATE PRODUCT
 const updateProductDetails = asyncHandler(async (req, res) => {
   try {
-
     const updatedProduct = await Product.findByIdAndUpdate(
       req.params.id,
       { ...req.fields },
@@ -48,4 +47,63 @@ const updateProductDetails = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { addProduct, updateProductDetails };
+// REMOVE PRODUCT
+const removeProduct = asyncHandler(async (req, res) => {
+  try {
+    const deleteProduct = await Product.findByIdAndDelete(req.params.id);
+    res.json(deleteProduct);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+// GET ALL PRODUCTS
+const fetchProducts = asyncHandler(async (req, res) => {
+  try {
+    const pageSize = 6;
+    const keyword = req.query.keyword
+      ? { name: { $regex: req.query.keyword, $options: "i" } }
+      : {};
+
+    const count = await Product.countDocuments({ ...keyword });
+    const products = await Product.find({ ...keyword }).limit(pageSize);
+
+    res.json({
+      products,
+      page: 1,
+      pages: Math.ceil(count / pageSize),
+      hasMore: false,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+// GET PRODUCT BY ID 
+const fetchProductById = asyncHandler(async (req, res) => {
+  try {
+    
+    const product = await Product.findById(req.params.id);
+
+    if (product){
+      return res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Product not found")
+    }
+
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ error: "Product not found" });
+  }
+});
+
+module.exports = {
+  addProduct,
+  updateProductDetails,
+  removeProduct,
+  fetchProducts,
+  fetchProductById
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const formidable = require("express-formidable");
-const { authenticate, authorizeAdmin } = require('../middlewares/authMiddleware');
+const {
+  authenticate,
+  authorizeAdmin,
+} = require("../middlewares/authMiddleware");
 const checkId = require("../middlewares/checkId");
 
 // controllers
-const { addProduct, updateProductDetails } = require("../controllers/productController");
+const {
+  addProduct,
+  updateProductDetails,
+  removeProduct,
+  fetchProducts,
+  fetchProductById,
+} = require("../controllers/productController");
 
 const router = express.Router();
 
-router.route('/').post(authenticate, authorizeAdmin, formidable(), addProduct);
-router.route('/:id').put(authenticate, authorizeAdmin, formidable(), updateProductDetails);
+router
+  .route("/")
+  .get(fetchProducts)
+  .post(authenticate, authorizeAdmin, formidable(), addProduct);
+router
+  .route("/:id")
+  .get(fetchProductById)
+  .put(authenticate, authorizeAdmin, formidable(), updateProductDetails)
+  .delete(authenticate, authorizeAdmin, removeProduct);
 
-module.exports = router
+module.exports = router;
